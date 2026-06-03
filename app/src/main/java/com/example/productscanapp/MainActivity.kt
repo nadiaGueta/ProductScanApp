2562b1c24a22512b5d857d3a0f1c5ee7f5526813
nadiaGueta <segma55@outlook.com>
2026-06-03 14:13:19 +0200

    ScanTestUS1

--- app/src/main/java/com/example/productscanapp/MainActivity.kt
+++ app/src/main/java/com/example/productscanapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.productscanapp.ui.scan.BarcodeScannerScreen
 import com.example.productscanapp.ui.theme.ProductScanAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProductScanAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                BarcodeScannerScreen(
+                    onBarcodeDetected = { barcode ->
+                        println("CODE DETECTE = $barcode")
+                    }
+                )
             }
         }
     }
